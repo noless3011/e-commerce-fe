faafeaa01a94c08844438048fd38f4179a043f27
noless3011 <botay301104@gmail.com>
2024-12-24 06:14:19 +0700

    basically finish the product and order page, just need to fix some api

--- src/api/api.ts
+++ src/api/api.ts
@@ -96,6 +96,12 @@ export interface ChatResponseDto {
      * @memberof ChatResponseDto
      */
     'image': Array<string>;
+    /**
+     * 
+     * @type {number}
+     * @memberof ChatResponseDto
+     */
+    'createTime': number;
 }
 
 export const ChatResponseDtoStatusEnum = {
@@ -195,7 +201,7 @@ export interface CreateProductDto {
      * @type {Array<string>}
      * @memberof CreateProductDto
      */
-    'image': Array<string>;
+    'images': Array<string>;
     /**
      * 
      * @type {number}
@@ -1034,6 +1040,36 @@ export const ChatApiAxiosParamCreator = function (configuration?: Configuration)
                 options: localVarRequestOptions,
             };
         },
+        /**
+         * 
+         * @summary Get list users in chat
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        chatControllerGetListUsers: async (options: RawAxiosRequestConfig = {}): Promise<RequestArgs> => {
+            const localVarPath = `/api/chat/list-user`;
+            // use dummy base URL string because the URL constructor only accepts absolute URLs.
+            const localVarUrlObj = new URL(localVarPath, DUMMY_BASE_URL);
+            let baseOptions;
+            if (configuration) {
+                baseOptions = configuration.baseOptions;
+            }
+
+            const localVarRequestOptions = { method: 'GET', ...baseOptions, ...options};
+            const localVarHeaderParameter = {} as any;
+            const localVarQueryParameter = {} as any;
+
+
+    
+            setSearchParams(localVarUrlObj, localVarQueryParameter);
+            let headersFromBaseOptions = baseOptions && baseOptions.headers ? baseOptions.headers : {};
+            localVarRequestOptions.headers = {...localVarHeaderParameter, ...headersFromBaseOptions, ...options.headers};
+
+            return {
+                url: toPathString(localVarUrlObj),
+                options: localVarRequestOptions,
+            };
+        },
     }
 };
 
@@ -1070,6 +1106,18 @@ export const ChatApiFp = function(configuration?: Configuration) {
             const localVarOperationServerBasePath = operationServerMap['ChatApi.chatControllerGet']?.[localVarOperationServerIndex]?.url;
             return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
         },
+        /**
+         * 
+         * @summary Get list users in chat
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        async chatControllerGetListUsers(options?: RawAxiosRequestConfig): Promise<(axios?: AxiosInstance, basePath?: string) => AxiosPromise<Array<UserResponseDto>>> {
+            const localVarAxiosArgs = await localVarAxiosParamCreator.chatControllerGetListUsers(options);
+            const localVarOperationServerIndex = configuration?.serverIndex ?? 0;
+            const localVarOperationServerBasePath = operationServerMap['ChatApi.chatControllerGetListUsers']?.[localVarOperationServerIndex]?.url;
+            return (axios, basePath) => createRequestFunction(localVarAxiosArgs, globalAxios, BASE_PATH, configuration)(axios, localVarOperationServerBasePath || basePath);
+        },
     }
 };
 
@@ -1099,6 +1147,15 @@ export const ChatApiFactory = function (configuration?: Configuration, basePath?
          */
         chatControllerGet(userId: number, options?: RawAxiosRequestConfig): AxiosPromise<Array<ChatResponseDto>> {
             return localVarFp.chatControllerGet(userId, options).then((request) => request(axios, basePath));
+        },
+        /**
+         * 
+         * @summary Get list users in chat
+         * @param {*} [options] Override http request option.
+         * @throws {RequiredError}
+         */
+        chatControllerGetListUsers(options?: RawAxiosRequestConfig): AxiosPromise<Array<UserResponseDto>> {
+            return localVarFp.chatControllerGetListUsers(options).then((request) => request(axios, basePath));
         },
     };
 };
@@ -1132,6 +1189,17 @@ export class ChatApi extends BaseAPI {
      */
     public chatControllerGet(userId: number, options?: RawAxiosRequestConfig) {
         return ChatApiFp(this.configuration).chatControllerGet(userId, options).then((request) => request(this.axios, this.basePath));
+    }
+
+    /**
+     * 
+     * @summary Get list users in chat
+     * @param {*} [options] Override http request option.
+     * @throws {RequiredError}
+     * @memberof ChatApi
+     */
+    public chatControllerGetListUsers(options?: RawAxiosRequestConfig) {
+        return ChatApiFp(this.configuration).chatControllerGetListUsers(options).then((request) => request(this.axios, this.basePath));
     }
 }
 
--- src/app/types/Product.tsx
+++ src/app/types/Product.tsx
@@ -14,6 +14,22 @@ export type ProductType =
     | "Furniture"
     | "Automotive"; // Extendable for other types
 
+interface ProductTypeColors {
+    bgColor: string;
+    textColor: string;
+}
+export const productTypeColorMap: Record<ProductType, ProductTypeColors> = {
+    Electronic: { bgColor: 'bg-blue-100', textColor: 'text-blue-800' },
+    Groceries: { bgColor: 'bg-green-100', textColor: 'text-green-800' },
+    Clothing: { bgColor: 'bg-purple-100', textColor: 'text-purple-800' },
+    HomeAppliances: { bgColor: 'bg-indigo-100', textColor: 'text-indigo-800' },
+    Books: { bgColor: 'bg-yellow-100', textColor: 'text-yellow-800' },
+    BeautyAndHealth: { bgColor: 'bg-pink-100', textColor: 'text-pink-800' },
+    SportsAndOurDoors: { bgColor: 'bg-teal-100', textColor: 'text-teal-800' },
+    ToysAndGames: { bgColor: 'bg-orange-100', textColor: 'text-orange-800' },
+    Furniture: { bgColor: 'bg-gray-100', textColor: 'text-gray-800' },
+    Automotive: { bgColor: 'bg-red-100', textColor: 'text-red-800' },
+};
 export default interface Product {
     id: string;
     created_at: string;
@@ -56,13 +72,36 @@ export const mapProductResponseToProduct = (
         createdTime: response.createdTime,
     };
 };
-
+export const defaultProduct: Product = {
+    id: "",
+    created_at: "",
+    updated_at: "",
+    status: "Available",
+    name: "",
+    description: "",
+    images: [],
+    price: 0,
+    discount: 0,
+    rating: 0,
+    remaining: 0,
+    soldNumber: 0,
+    totalLike: 0,
+    totalReview: 0,
+    ownerId: 0,
+    types: [],
+    createdTime: 0,
+};
+export const mapProductResponseArrayToProductArray = (
+    responses: ProductResponseDto[]
+): Product[] => {
+    return responses.map(response => mapProductResponseToProduct(response));
+};
 
 export function convertProductToCreateProductDto(product: Product): CreateProductDto {
     const createProductDto: CreateProductDto = {
         name: product.name,
         description: product.description,
-        image: product.images,
+        images: product.images,
         price: product.price,
         discount: product.discount,
         rating: product.rating,
