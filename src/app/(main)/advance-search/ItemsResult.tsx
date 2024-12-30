871d6a2eca56258b6388ed5afd195222b9bc8374
Hoang254-emat <c2lieuxadobahoang@gmail.com>
2024-12-30 19:54:55 +0700

    da xu ly xong thanh search va hien thi san pham

--- src/app/(main)/advance-search/ItemsResult.tsx
+++ src/app/(main)/advance-search/ItemsResult.tsx
@@ -17,22 +17,24 @@ const ItemsResult: React.FC<ItemsResultProps> = ({ results }) => {
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {results.map((product) => (
-                <div key={product.id} className="relative w-full h-64 bg-white p-4 rounded-lg shadow-lg">
-                    {/* Hiển thị hình ảnh sản phẩm */}
-                    {product.images.length > 0 ? (
-                        <Image
-                            src={product.images[0]} // Hiển thị ảnh đầu tiên trong mảng images
-                            alt={product.name}
-                            layout="fill"
-                            objectFit="cover"
-                            className="rounded-md mb-4"
-                            crossOrigin='anonymous'
-                        />
-                    ) : (
-                        <div className="w-full h-64 bg-gray-300 rounded-md mb-4 flex items-center justify-center">
-                            No Image
-                        </div>
-                    )}
+                <div key={product.id} className="relative w-full bg-white p-4 rounded-lg shadow-lg">
+                    {/* Container cố định kích thước cho ảnh */}
+                    <div className="relative w-full h-40 mb-4 overflow-hidden">
+                        {product.images.length > 0 ? (
+                            <Image
+                                src={product.images[0]} // Hiển thị ảnh đầu tiên
+                                alt={product.name}
+                                layout="fill"
+                                objectFit="cover"
+                                className="rounded-md"
+                                crossOrigin="anonymous"
+                            />
+                        ) : (
+                            <div className="w-full h-full bg-gray-300 flex items-center justify-center">
+                                No Image
+                            </div>
+                        )}
+                    </div>
 
                     {/* Thông tin sản phẩm */}
                     <h3 className="text-xl font-semibold text-gray-800 mb-2">{product.name}</h3>
